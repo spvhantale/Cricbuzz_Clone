c450666756009d91e7f56476e6880bcfbbb825a9
spvhantale <vhantale510@gmail.com>
2022-07-23 00:16:37 +0530

    completed topic

--- cricbuzzplus.js
+++ cricbuzzplus.js
@@ -21,7 +21,7 @@ cricbuzzplus()
     const option = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '8cfec7ed71msh5171ae94d556119p1514a6jsnec75adf16030',
+		'X-RapidAPI-Key': '2eabeba429msh7fcbdf39ed18ec5p15a68cjsn68ae23da56c5',
 		'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com'
 	}
 };
--- topic.js
+++ topic.js
@@ -16,22 +16,22 @@ let clickFunc = () => {
 
 document.querySelector("#newsnavbar").innerHTML=navbarlowers();
 clickFunc();
-allstories();
+topicslower();
     const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '2eabeba429msh7fcbdf39ed18ec5p15a68cjsn68ae23da56c5',
+		'X-RapidAPI-Key': '412ee806edmsh2fac8ee2a346dabp1b7d50jsnb24cdfea69db',
 		'X-RapidAPI-Host': 'unofficial-cricbuzz.p.rapidapi.com'
 	}
 };
-    fetch('https://unofficial-cricbuzz.p.rapidapi.com/news/list', options)
+    fetch('https://unofficial-cricbuzz.p.rapidapi.com/news/get-topics', options)
 	.then(response => response.json())
-	.then(response => append(response.newsList))
+	.then(response =>append(response.topics))
 	.catch(err => console.error(err));
     const option = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '8cfec7ed71msh5171ae94d556119p1514a6jsnec75adf16030',
+		'X-RapidAPI-Key': '2eabeba429msh7fcbdf39ed18ec5p15a68cjsn68ae23da56c5',
 		'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com'
 	}
 };
@@ -42,39 +42,14 @@ allstories();
     let append=(array)=>{
     let allstories=document.querySelector("#allstories");
     array.forEach(element => {
-        if(element.story==undefined){
-            return 
-        }else{
-            // console.log(element.story)
-            let div=document.createElement("div");
-            div.addEventListener("click",function(){
-                console.log("Swapnil")
-                localStorage.setItem("newspage",JSON.stringify(element));
-                window.location.href="newspage.html";
-            })
-            let div1=document.createElement("div");
-            console.log(element);
-            div.setAttribute("class","newsallstories")
-            let p=document.createElement("p");
-            p.innerText=`${element.story.storyType}.${element.story.context}`;
-            p.style.color="gray";
-            p.style.marginTop="-1px"
-            p.style.textTransform="uppercase";
-            let img=document.createElement("img");
-            img.style.borderRadius="5px";
-            // img.style.height="150px";
-            img.src=`https://www.cricbuzz.com/a/img/v1/205x152/i1/c${element.story.coverImage.id}/dunkley-cracked-a-quick-fire-f.jpg`
-            let h2=document.createElement("h3");
-            h2.innerText=element.story.hline;
-            let p2=document.createElement("p");
-            p2.innerText=element.story.intro;
-            let hr=document.createElement("hr");
-            div1.append(p,h2,p2);
-            div.append(img,div1);
-            allstories.append(div,hr);
-        }  
+          let h1=document.createElement("h2");
+          h1.innerText=element.headline
+           let p=document.createElement("p");
+           p.innerText=element.desc;
+           allstories.append(h1,p);
     });
 }
+
 let append2=(array)=>{
     
     let latestphots=document.querySelector("#latestphots");
